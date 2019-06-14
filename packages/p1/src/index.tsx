b9e813a51aadc29736d65b5d0e66bee50d3b493e
bhy <450215151@qq.com>
2019-06-14 14:14:55 +0800

    测试

--- packages/p1/src/index.tsx
+++ packages/p1/src/index.tsx
@@ -3,10 +3,10 @@ import * as React from 'react'
 // @ts-ignore
 import s from './style.scss'
 export default function (): any {
-  return (
-        <div className={s.p1}>
+                    return (
+              <div className={s.p1}>
             p1
-            <P2 />
+              <P2ddddddddddddddd />
         </div>
   )
 }
