ab768d711d70a34cd01c82e934028c0ddfc2be63
bhy <450215151@qq.com>
2019-06-21 15:03:47 +0800

    美化代码

--- stories/p1/index.stories.tsx
+++ stories/p1/index.stories.tsx
@@ -5,16 +5,18 @@ import P1 from '../../packages/p1/src/index'
 
 storiesOf('p1', module)
   .addDecorator(withKnobs)
-  .add('demo', () => {
-    const v = text('value', 'ddd')
+  .add(
+    'demo',
+    () => {
+      const v = text('value', 'ddd')
 
-    return (
-      <div>
-        <P1 value={v} />
-      </div>
-    )
-  },
-  {
-    jest: ['p1']
-  }
+      return (
+        <div>
+          <P1 value={v} />
+        </div>
+      )
+    },
+    {
+      jest: ['p1'],
+    },
   )
