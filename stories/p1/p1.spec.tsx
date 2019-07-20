09da378428b62d54a03a9437b07d41e5981629d1
bhy <450215151@qq.com>
2019-07-20 15:18:10 +0800

    p1单元测试改成mocha

--- stories/p1/p1.spec.tsx
+++ stories/p1/p1.spec.tsx
@@ -1,3 +1,4 @@
+import { assert } from 'chai'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import * as React from 'react'
@@ -7,14 +8,15 @@ import s from '../../packages/p1/src/style.scss'
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('all', () => {
-  test('input', () => {
+  it('input', () => {
     const props: IProps = {
       value: 'dddd',
     }
     const p1 = shallow(<P1 {...props} />)
-    expect(p1.find('h1').text()).toEqual('dddd')
+    assert.strictEqual(p1.find('h1').text(), 'dddd')
 
     p1.find('.' + s.p1__change).simulate('click')
-    expect(p1.find('h3').text()).toEqual('hehehehehe')
+
+    assert.strictEqual(p1.find('h3').text(), 'hehehehehe')
   })
 })
