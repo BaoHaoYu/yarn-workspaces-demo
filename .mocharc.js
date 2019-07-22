20d8324626525a767bd9e449bd29ffba52aa929e
bhy <450215151@qq.com>
2019-07-22 14:10:26 +0800

    fixed: mocha无法找到document

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extension: ['tsx'],
   spec: 'stories/**/*.spec.tsx',
-  require: ['esm', 'ts-node/register', 'ignore-styles'],
+  require: ['esm', 'setup.js', 'ts-node/register', 'ignore-styles'],
   // compilers: 'js:babel-core/register,css:css-dnt-compiler.js'
 }
